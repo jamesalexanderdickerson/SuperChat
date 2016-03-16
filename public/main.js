efc5925cf8301eff5ed1432c3d6500f71fdce024
jamesalexanderdickerson <jamesalexanderdickerson@gmail.com>
2016-03-16 19:32:44 -0400

    fixed scroll issue

--- public/main.js
+++ public/main.js
@@ -6,10 +6,7 @@
   app.controller('HomeCtrl', function($scope, $http) {
     $http.get('/')
       .success(function(data) {
-        $scope.scrollJump = function () {
-          var chatWindow = document.getElementById("messages")
-          chatWindow.scrollTop = chatWindow.scrollHeight
-        }
+
       })
     $http.get('/chat')
        .success(function(data) {
